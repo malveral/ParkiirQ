d1db12aa3f2f039ea259d65e572e1a39b30065fb
michellealvera <malveralolang@student.ciputra.ac.id>
2024-07-31 08:03:33 +0700

    Initial Commit

---
+++ ParkiirQ/ParkiirQApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParkiirQApp.swift
+//  ParkiirQ
+//
+//  Created by Michelle Alvera Lolang on 31/07/24.
+//
+
+import SwiftUI
+
+@main
+struct ParkiirQApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
